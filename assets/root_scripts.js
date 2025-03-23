135e95c70f36cdf3b87b4ce38554627b5417cf67
Akram Hamed <akhamed@yappit.org>
2025-03-23 09:50:23 +0200

    optimized

--- assets/root_scripts.js
+++ assets/root_scripts.js
@@ -3,19 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const navbarCollapse = document.querySelector(".navbar-collapse");
 
   navLinks.forEach(function (link) {
-    link.addEventListener("click", function () {
-      if (navbarCollapse.classList.contains("show")) {
-        new bootstrap.Collapse(navbarCollapse).toggle();
-      }
-    });
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const navLinks = document.querySelectorAll(".navbar-collapse .nav-link");
-  const navbarCollapse = document.querySelector(".navbar-collapse");
-
-  navLinks.forEach(function (link) {
     link.addEventListener("click", function (e) {
       const targetId = link.getAttribute("data-target");
       if (targetId) {
